28dcdda9d5ab1c08375fbb0d9e0b9571b2a91c44
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-10 11:08:23 +0530

    Add files via upload

---
+++ Luckyseven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Luckyseven
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String  S=sc.nextLine();
+		System.out.println(S.charAt(6));
+
+	}
+}
