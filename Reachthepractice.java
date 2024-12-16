e3e921d10a36929df1ea60bb6950398837706c31
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 07:06:21 +0530

    Add files via upload

---
+++ Reachthepractice.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Reachthepractice
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int t=sc.nextInt();
+	 for(int i=0;i<t;i++){
+	     int X=sc.nextInt();
+	     int Y=sc.nextInt();
+	     int c=X-Y;
+	     System.out.println(c);
+	 }
+
+	}
+}
