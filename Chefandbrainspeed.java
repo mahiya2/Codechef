405224e85b8c42ed92ab5d034328ea50a61fd919
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 10:12:45 +0530

    Add files via upload

---
+++ Chefandbrainspeed.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class Chefandbrainspeed
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(y>x)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+	}
+}
