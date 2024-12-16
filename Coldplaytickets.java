a85540f5e96c0ed7fe7c1ced376b7d6b9bfbf886
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 10:33:18 +0530

    Add files via upload

---
+++ Coldplaytickets.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Coldplaytickets
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int c=N+1;
+       System.out.println(c*5000);
+	}
+}
