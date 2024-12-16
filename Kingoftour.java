ffadf5a7bf4a7c1d28a961aa1e94dec2e7171b7b
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 07:15:49 +0530

    Add files via upload

---
+++ Kingoftour.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Kingoftour
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		    for (int i=0;i<t;i++){
+		        int N=sc.nextInt();
+		        int M=sc.nextInt();
+		        System.out.println((N*5)+(M*7));
+		    }
+		}
+	}
+
