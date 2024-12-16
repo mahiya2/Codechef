ef857b9931c27057d1b418324eac3e698b48a784
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 07:25:54 +0530

    Add files via upload

---
+++ Heatwave.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Heatwave
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int X=sc.nextInt();
+	 int Y=sc.nextInt();
+	 if(X<Y){
+	     System.out.println("NO");
+	 }
+	 else{
+	     System.out.println("YES");
+	 }
+	}
+}
