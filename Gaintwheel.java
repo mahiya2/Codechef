8037bc6df6f9bb6ff85652627e72c083cfb3a247
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 10:37:20 +0530

    Add files via upload

---
+++ Gaintwheel.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Gaintwheel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X>=60)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
