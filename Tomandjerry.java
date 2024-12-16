538e3c87008cf6854cd3fff71e8b95529ce6b4e7
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 11:00:12 +0530

    Add files via upload

---
+++ Tomandjerry.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Tomandjerry
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    if(X<Y)
+		    System.out.println("YES");
+		    else System.out.println("NO");
+		}
+	}
+}
