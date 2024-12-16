d55f06f8dfb5f1545dffbd65add695e297a80a99
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-16 10:47:54 +0530

    Add files via upload

---
+++ Countingwords.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Countingwords
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int N=sc.nextInt();
+		    int M=sc.nextInt();
+		    int a=N*M;
+		    System.out.println(a);
+		}
+	}
+}
