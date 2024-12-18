714ac062bedbf433ec75e704b187fe7a35af63e6
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-18 18:16:11 +0530

    Add files via upload

---
+++ Agening.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Agening
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    int y=X-10;
+		    System.out.println (y);
+		}
+
+	}
+}
