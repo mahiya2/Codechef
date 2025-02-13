4da5dc4f484b8f02bef3177c9c79b25af407cbf9
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:11:33 +0530

    Add files via upload

---
+++ Sumordiff.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N1=sc.nextInt();
+		int N2=sc.nextInt();
+		int a=N1-N2;
+		int b=N1+N2;
+		if(N1>N2){
+		System.out.println(a);
+}
+else
+{
+    System.out.println(b);
+}
+	}
+}
