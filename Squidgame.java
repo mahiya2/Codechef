361cd9fc96a250aa36c84901303faf7373b37d6b
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:13:48 +0530

    Add files via upload

---
+++ Squidgame.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		int a=N-K;
+		int b=a*10000;
+		System.out.println(b);
+
+	}
+}
