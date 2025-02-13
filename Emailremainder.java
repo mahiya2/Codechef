4c8a2e0056dd2955058a74b9de6f273d126abe5f
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:59:23 +0530

    Add files via upload

---
+++ Emailremainder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int U=sc.nextInt();
+		int c=N-U;
+		System.out.println(c);
+		
+
+	}
+}
