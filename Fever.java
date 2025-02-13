cfa38ab118a0e80d10cffa9f2085ef80d5e30585
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 20:16:25 +0530

    Add files via upload

---
+++ Fever.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in );
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    if(X>98){
+		        System.out.println("YES");
+		    }
+		    else
+		    System.out.println("NO");
+		}
+	}
+}
