2436974e6f1faf2ab7da46cedc5e2de7c1e38aaf
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 19:21:36 +0530

    Add files via upload

---
+++ Goodinvestment.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++){
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        int c=Y*2;
+        if(X>=c){
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+	}
+}
