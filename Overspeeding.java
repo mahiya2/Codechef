3fae0c95d205ff2636f3b3e7abbb4b3b1e53a91a
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 19:36:46 +0530

    Add files via upload

--- Overspeeding.java
+++ Overspeeding.java
@@ -1,15 +1,29 @@
 import java.util.*;
-class Overspeeding
+import java.lang.*;
+import java.io.*;
+
+class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int S=sc.nextInt();
-		if(S<=40){
-		    System.out.println("NO");
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    if(X<=70){
+		        System.out.println("0");
+		    }
+		    else if(70<X&&X<=100){
+		        System.out.println("500");
+		    }
+		    else if(X>100){
+		        System.out.println("2000");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
 		}
-		else{
-		    System.out.println("YES");
-		}
+
 	}
 }
