b2ca52d79bda2c81568f3ef50a6d7cfa49518228
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 11:47:39 +0530

    Add files via upload

---
+++ Oddsandevens.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=a+b;
+		    if(c%2==0){
+		        System.out.println("Bob");
+		    }
+		    else
+		    {
+		        System.out.println("Alice");
+		    }
+		}
+
+	}
+}
