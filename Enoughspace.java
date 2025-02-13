db64951e9e7afd1ecc7f41a3046d8a6d68a88178
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 20:22:53 +0530

    Add files via upload

---
+++ Enoughspace.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int N=sc.nextInt();
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    int c=X+Y*2;
+		    if(N>=c){
+		        System.out.println("YES");
+		    }
+		    else
+		    System.out.println("NO");
+		}
+	}
+}
