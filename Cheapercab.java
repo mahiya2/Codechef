29845fa7928e8641d408c6d628a338acafd8b727
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:43:47 +0530

    Add files via upload

---
+++ Cheapercab.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    if(X<Y){
+		        System.out.println("FIRST");
+		    }
+		    else if(X==Y){
+		        System.out.println("ANY");
+		    }
+		    else
+		    System.out.println("SECOND");
+		}
+
+	}
+}
