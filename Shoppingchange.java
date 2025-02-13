288508d9f69d6f76b9b6fda52901122292d335e3
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:21:47 +0530

    Add files via upload

---
+++ Shoppingchange.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    int a=100-X;
+		    System.out.println(a);
+		}
+
+	}
+}
