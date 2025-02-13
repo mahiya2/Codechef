c701c487426c021595524a4eef6e844a3d481a24
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:31:30 +0530

    Add files via upload

---
+++ Aianalysis.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int C=sc.nextInt();
+	if(C<=1000){
+	    System.out.println("Yes");
+	}
+	else
+	{
+	    System.out.println("No");
+	}
+
+	}
+}
