ea006c2a652c669d0226ade6b8270f14b5cd5585
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 20:24:27 +0530

    Add files via upload

---
+++ Valentinescontest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N==121){
+		    System.out.println("Likely");
+		}
+		else
+		{
+		    System.out.println("Unlikely");
+		}
+
+	}
+}
