3209cc52a4432d40f567f14a88d66475a757311d
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 19:18:41 +0530

    Add files via upload

---
+++ Footballtraining.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+       Scanner sc=new Scanner(System.in);
+       int X=sc.nextInt();
+       int Y=sc.nextInt();
+       if(X>Y){
+           System.out.println("FREEKICK");
+       }
+       else
+       System.out.println("PENALTY");
+       }
+    
+}
