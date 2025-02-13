bbba6a28cb0ce3b816b96dadc3e01d5a5f982229
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 20:21:31 +0530

    Add files via upload

---
+++ Waitingtime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in );
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int K=sc.nextInt();
+		    int X=sc.nextInt();
+		    int d=K*7-X;
+		    System.out.println(d);
+		}
+
+	}
+}
