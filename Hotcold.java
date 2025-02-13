8878e61af8664ae28693fc4b960dc56c43c5bba8
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-13 12:34:38 +0530

    Add files via upload

---
+++ Hotcold.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int C=sc.nextInt();
+		    if(C>20){
+		        System.out.println("HOT");
+		    }
+		    else
+		    System.out.println("COLD");
+		}
+
+	}
+}
