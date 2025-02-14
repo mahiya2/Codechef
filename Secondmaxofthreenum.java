7342167dbad868a700a01499a5b618f4206c3ea2
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-14 11:53:54 +0530

    Add files via upload

---
+++ Secondmaxofthreenum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    if((a>=b && a<c) || (a>=c && a<b)){
+		        System.out.println(a);
+		    }
+		    else if((b>=c && b<a) || (b>=a && b<c)){
+		        System.out.println(b);
+		    }
+		    else{
+		        System.out.println(c);
+		    }
+		    
+		    
+		}
+
+	}
+}
