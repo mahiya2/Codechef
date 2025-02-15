e93aa958c28670cdda7eee38dcdb0112ee41ad5a
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-16 00:22:24 +0530

    Add files via upload

---
+++ Worthofavideo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      for(int i=0;i<t;i++){
+          int S=sc.nextInt();
+          int a=24;
+          int b=S*a;
+          int c=b*1000;
+          System.out.println(c);
+      }
+
+	}
+}
