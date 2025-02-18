dc76c5208e5b7bacc6f963036b6bb33f136927a3
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-18 18:41:34 +0530

    Add files via upload

---
+++ Televisionchannels.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int X=sc.nextInt();
+	  int a=X/2;
+	  if(X%2==0){
+	      System.out.println(a);
+	  }
+else
+System.out.println(a+1);
+	}
+}
