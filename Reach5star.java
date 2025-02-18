c27929f9a41379fc6da024ae9d75d3abeb408ae4
mahiya2 <fazeelamuskan12@gmail.com>
2025-02-18 18:42:52 +0530

    Add files via upload

---
+++ Reach5star.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      int X=sc.nextInt();
+      int Y=sc.nextInt();
+      int a=X+Y;
+      if(a>=2000){
+          System.out.println("YES");
+      }
+      else
+      System.out.println("NO");
+
+	}
+}
