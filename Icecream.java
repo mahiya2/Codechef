a391128b3f953b7f1e86c411f9d7dc6c316ea750
mahiya2 <fazeelamuskan12@gmail.com>
2025-03-19 20:58:33 +0530

    Add files via upload

---
+++ Icecream.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+          Scanner sc=new Scanner(System.in);
+              int X=sc.nextInt();
+              int Y=sc.nextInt();
+              int c=2*X;
+              if(Y>=c){
+                  System.out.println("YES");
+              }
+              else
+              {
+                  System.out.println("NO");
+              }
+          }
+	}
