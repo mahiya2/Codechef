8c2f5b4f845e63353174f3a5f768904b87a36e31
mahiya2 <fazeelamuskan12@gmail.com>
2025-03-20 21:25:17 +0530

    Add files via upload

---
+++ Trianglechecking.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    int C=sc.nextInt();
+		    if(A + B > C && B + C > A && A + C >B){
+		        System.out.println("Yes");
+		    }
+		    else
+		    System.out.println("No");
+		}
+
+	}
