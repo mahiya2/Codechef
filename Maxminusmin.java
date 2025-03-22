a265ba3229f23b2a11de141852ce629273140750
mahiya2 <fazeelamuskan12@gmail.com>
2025-03-22 12:09:27 +0530

    Add files via upload

---
+++ Maxminusmin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    for(int i=0;i<t;i++){
+	        int A=sc.nextInt();
+	        int B=sc.nextInt();
+	        int C=sc.nextInt();
+	        int d=C-A;
+	        System.out.println(d);
+	    }
+ sc.close();
+	}
+}
