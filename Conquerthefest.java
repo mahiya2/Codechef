c47f7f3f766d82c4fcd103c3f5f2a5307ac6abe6
mahiya2 <fazeelamuskan12@gmail.com>
2025-03-27 20:21:54 +0530

    Add files via upload

---
+++ Conquerthefest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int B=sc.nextInt();
+		int c=10*B;
+		if(N>=c){
+		    System.out.println("YES");
+		}
+		else
+		System.out.println("NO");
+
+	}
+}
