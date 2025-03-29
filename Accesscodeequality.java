290a4d8247de4ce2187d650fb19218ddc5dc9fb1
mahiya2 <fazeelamuskan12@gmail.com>
2025-03-29 12:45:52 +0530

    Add files via upload

---
+++ Accesscodeequality.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scanner =new Scanner(System.in);
+	    String S = scanner.next();
+    if(S.equals("WECNITK")){
+        System.out.println("Welcome to Web Club!");
+    }
+    else
+    System.out.println("Access denied");
+    
+	}
+}
